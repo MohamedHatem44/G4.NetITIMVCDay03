287ad732b310832f933580f59b78c3f503166a89
Mohamed Hatem <mohamed.hatem157@example.com>
2024-08-24 12:41:34 +0300

    Add project files.

---
+++ G4.NetITIMVCDay03/Controllers/DepartmentController.cs
@@ -0,0 +1,46 @@
+﻿using G4.NetITIMVCDay03.Context;
+using G4.NetITIMVCDay03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G4.NetITIMVCDay03.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        /*---------------------------------------------------------*/
+        // Context
+        ComapnyContext comapnyContext = new ComapnyContext();
+        /*---------------------------------------------------------*/
+        // Get All => Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //var departments = comapnyContext.Departments.ToList(); // Cast To List
+            var departments = comapnyContext.Departments;
+            ViewBag.Departments = departments;
+            return View();
+        }
+        /*---------------------------------------------------------*/
+        [HttpGet]
+        public IActionResult ViewDetails(int id)
+        {
+            var department = comapnyContext.Departments.Find(id);
+            ViewBag.Department = department;
+            return View();
+        }
+        /*---------------------------------------------------------*/
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        /*---------------------------------------------------------*/
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            comapnyContext.Departments.Add(department);
+            comapnyContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        /*---------------------------------------------------------*/
+    }
+}
---
+++ G4.NetITIMVCDay03/Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using G4.NetITIMVCDay03.Context;
+using G4.NetITIMVCDay03.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace G4.NetITIMVCDay03.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        /*---------------------------------------------------------*/
+        // Context
+        ComapnyContext comapnyContext = new ComapnyContext();
+        /*---------------------------------------------------------*/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var Employees = comapnyContext.Employees.Include(e => e.Department);
+            return View(Employees);
+        }
+        /*---------------------------------------------------------*/
+        [HttpGet]
+        public IActionResult ViewDetails(int id)
+        {
+            var Employee = comapnyContext.Employees.Include(e => e.Department).FirstOrDefault(e => e.Id == id);
+            return View(Employee);
+        }
+        /*---------------------------------------------------------*/
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Departments = new SelectList(comapnyContext.Departments, "Id", "DeptName");
+            return View();
+        }
+        /*---------------------------------------------------------*/
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            comapnyContext.Employees.Add(employee);
+            comapnyContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        /*---------------------------------------------------------*/
+    }
+}
